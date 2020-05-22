25b7ac557b016b878bd62d82c29ac9bf0c641bd8
Laks96 <lazarspasic96@gmail.com>
2020-05-22 12:54:00 +0200

    Adding CDN for materialize css

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header/Header'
+
+
+class Layout extends React.Component {
+
+
+    render() {
+        return <>
+        <Header />
+            <main>
+                {this.props.children}
+            </main>
+        <Footer />
+
+        </>
+    }
+}
+
+export default Layout
