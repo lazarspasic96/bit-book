4fd6e074950edc93fef4e6e534b3e1d583d41d14
Laks96 <lazarspasic96@gmail.com>
2020-06-17 20:59:34 +0200

    Apply some style for Favourite posts

--- src/containers/Dashboard/Favourite/FavouritePosts.js
+++ src/containers/Dashboard/Favourite/FavouritePosts.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PostCard from '../../../components/PostLayout/PostCard/PostCard'
+import classes from './FavouritePosts.module.css'
 
 class Favourite extends React.Component {
     render () {
         const favouritePosts = this.props.favouritePosts.filter(post => post.fav).map(post => <PostCard postData = {post}/>)
         return <>
         <p>eee</p>
-        {favouritePosts}
+        <div className = {classes.FavouritePosts}>
+                  {favouritePosts}
+        </div>
+  
          </>
 
     }
